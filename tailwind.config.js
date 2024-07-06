b8b82103866272bdeba6d47c8f97a7015112a354
Luis Amador <amador@panorama.works>
2024-07-05 23:18:27 -0600

    mobile markup done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        sinigreen: "#DFEADD",
+        siniblue: "#0E2F4B",
+        siniemerald: "#00A4A0",
+      },
+    },
+  },
+  plugins: [],
+}
